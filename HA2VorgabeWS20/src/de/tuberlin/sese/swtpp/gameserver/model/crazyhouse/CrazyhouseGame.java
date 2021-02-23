743387ba4105c98814cc0ec07f35b5af13cdc839
Adrians23 <iadrianescu@gmail.com>
2021-02-23 20:58:23 +0100

    crazyhousegame
    I just used the code I had from last semester - DeathStacksGame. It can:
    -translate FEN to GameBoard Array [8][8] - I need to add case of last line about on side pieces;
    -read given move String and perform the changes on the board. Only works for strings like "A1-A3".   -- I need to implement the special case: "r-c4"
    -translate Board into state in form of FEN String.
    
    To do:
    -chess/crazyhose moves logic,

--- HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/CrazyhouseGame.java
+++ HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/CrazyhouseGame.java
@@ -1,9 +1,12 @@
 package de.tuberlin.sese.swtpp.gameserver.model.crazyhouse;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 import de.tuberlin.sese.swtpp.gameserver.model.Game;
 import de.tuberlin.sese.swtpp.gameserver.model.Player;
+import de.tuberlin.sese.swtpp.gameserver.model.Move;
+
 
 public class CrazyhouseGame extends Game implements Serializable{
 
@@ -22,13 +25,25 @@ public class CrazyhouseGame extends Game implements Serializable{
 
 	// internal representation of the game state
 	// TODO: insert additional game data here
+	private String state;
+	private String[][] gameBoard = new String [][]{{"R", "N", "B", "Q", "K", "B", "N", "R"}, 
+        {"P", "P", "P", "P", "P", "P", "P", "P"},
+        {"-", "-", "-", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "-", "-", "-", "-"},
+        {"p", "p", "p", "p", "p", "p", "p", "p"},
+        {"r", "n", "b", "q", "k", "b", "n", "r"}};;
+
+
 
 	/************************
 	 * constructors
 	 ***********************/
 
-	public CrazyhouseGame() {
-		super();
+	public CrazyhouseGame() throws Exception{
+		super();	
+		
 
 		// TODO: initialize internal model if necessary 
 	}
@@ -205,22 +220,181 @@ public class CrazyhouseGame extends Game implements Serializable{
 		// Note: This method is for automatic testing. A regular game would not start at some artificial state.
 		//       It can be assumed that the state supplied is a regular board that can be reached during a game.
 		// TODO: implement
+        String temp3[] = state.split("/");       
+        for(int i=0;i<8;i++){
+            String str = temp3[i];
+            int k = 0;
+            for(int j=0;j<str.length();j++){                   
+                char code = str.charAt(j);  
+                String cod = String.valueOf(code);
+                boolean numeric = true;
+                
+                try {
+                    int num = Integer.parseInt(cod);
+                } catch (NumberFormatException e) {
+                    numeric = false;
+                }
+               
+                if(numeric){
+                    int num = Integer.parseInt(cod);
+                      for(int count=0;count<num;count++){
+                        gameBoard[7-i][k] = "-";
+                        k++;
+                      }                          
+                }                                       
+                else{                        
+                    gameBoard[7-i][k] = cod;
+                    k++;
+                }             
+        }
+        }	
 	}
 
 	@Override
+	//returns String reprasentation of state 
 	public String getBoard() {
 		// TODO: implement
 		
 		// replace with real implementation
-		return "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/";
+		//return "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/";
+		updateState();
+		return state;
 	}
 
 	@Override
 	public boolean tryMove(String moveString, Player player) {
 		// TODO: implement
+		CHMove m = new CHMove(moveString, state, player);
+		//boolean obeyTooTallRule = isTooTallRule(m);
+		//if (m.isMoveLegit() && isPlayerCheckerOnTop(m.getFrom()) && obeyTooTallRule)
+		if (m.isMoveLegit())
+	
+			
+		{
+			System.out.println(moveString);
+			updateBoard(m);
+			updateState();
+			System.out.println(state);
+			/*
+			if (checkIfWon(isWhiteNext()))
+			{
+				endGame();
+			}
+			*/
+
+			if(isWhiteNext())
+				setNextPlayer(blackPlayer);
+			else
+				setNextPlayer(whitePlayer);
+			
+			return true;
+		}
+				
 		
 		// replace with real implementation
 		return false;
 	}
+	
+	
+	//
+	
+	//need to work on it, sometimes produces rows of "1" instead of sum of empty boxes
+	
+	private void updateState() {
+		state = "";
+		for(int j = 7 ; j >= 0 ; j--){
+			int counter=0;
+			for(int i = 0 ; i < 8 ; i++)
+				
+			{
+				String temp ="";
+				String temp2 = "";
+				temp=gameBoard[j][i].toString();
+				if(i<7) {
+				temp2=gameBoard[j][i+1].toString();
+				}
+				if(temp.equals("-")){
+					counter++;
+					if(i==7||temp2!="-") {
+					state+=counter;
+					counter=0;
+					}
+				}
+				else {
+		                state += gameBoard[j][i];
+		                
+		            }
+		
+					
+			}
+			state += "/";
+		}
+	}
+	
+	
+	
+	private void updateCells(int fromCol, int fromSeg, int toCol, int toSeg) {
+		// remove
+					String toAdd = "";
+					
+					toAdd += gameBoard[fromSeg-1][fromCol].charAt(0);
+					
+					StringBuilder sb = new StringBuilder(gameBoard[fromCol][fromSeg-1]);
+					sb.deleteCharAt(0);
+					sb.insert(0,"-");
+					
+					
+					
+
+					gameBoard[fromSeg-1][fromCol] = sb.toString();
+					//add
+					StringBuilder sb1 = new StringBuilder(gameBoard[toSeg-1][toCol]);
+					// put on top
+					sb1.deleteCharAt(0);
+					sb1.insert(0, toAdd);
+
+					gameBoard[toSeg-1][toCol] = sb1.toString();
+					
+	
+	}
+	
+	
+	public void updateBoard(CHMove m) {
+		
+		
+		// get cells info (column from 0, row from 1)
+		int fromColumnNumber = (int)m.getFrom().charAt(0) - 97;
+
+		int fromRowSegment = Integer.parseInt(m.getFrom().charAt(1)+"");
+
+
+		
+		int toColumnNumber = (int)m.getTo().charAt(0) - 97;
+
+		int toRowSegment = Integer.parseInt(m.getTo().charAt(1)+"");
+
+		
+		// update cells on board
+		updateCells(fromColumnNumber, fromRowSegment, toColumnNumber, toRowSegment);
+	}
+
+	
+	/*
+	private void initGameBoard()
+	{
+		gameBoard = new String [][]{{"R", "N", "B", "Q", "K", "B", "N", "R"}, 
+		             {"P", "P", "P", "P", "P", "P", "P", "P"},
+		             {"-", "-", "-", "-", "-", "-", "-", "-"},
+		             {"-", "-", "-", "-", "-", "-", "-", "-"},
+		             {"-", "-", "-", "-", "-", "-", "-", "-"},
+		             {"-", "-", "-", "-", "-", "-", "-", "-"},
+		             {"p", "p", "p", "p", "p", "p", "p", "p"},
+		             {"r", "n", "b", "q", "k", "b", "n", "r"}};
+		
+	
+	}
+*/
+	
+	
 
 }
